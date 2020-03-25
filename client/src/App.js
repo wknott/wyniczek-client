1c714011a4ba97685a2ca47e1a2b405d02766dc9
Wojciech Knott <wyoyo7@gmail.com>
2020-03-25 09:14:02 +0100

    Add Menu component

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Wyniczek from "./components/Wyniczek"
-import EdycjaWynikow from "./components/EdycjaWynikow"
-import StworzWynik from "./components/StworzWynik"
-
+import Wyniczek from "./components/Wyniczek";
+import EdycjaWynikow from "./components/EdycjaWynikow";
+import StworzWynik from "./components/StworzWynik";
+import Menu from "./components/Menu";
 function App() {
   return (
-    <Router>
-      <Route path="/" exact component={Wyniczek}/>
-      <Route path="/edit/:id" component={EdycjaWynikow}/>
-      <Route path="/create" component={StworzWynik}/>
-    </Router>
+      <Router>
+        <div className="container">
+          <Menu/>
+          <Route path="/" exact component={Wyniczek}/>
+          <Route path="/edit/:id" component={EdycjaWynikow}/>
+          <Route path="/create" component={StworzWynik}/>
+        </div>
+      </Router>
   );
 }
 
