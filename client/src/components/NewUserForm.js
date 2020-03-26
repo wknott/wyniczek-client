dea1c748574ce590ff4dff5f5f1e9b7c45006734
Wojciech Knott <wyoyo7@gmail.com>
2020-03-26 18:04:17 +0100

    Add server project

--- client/src/components/NewUserForm.js
+++ client/src/components/NewUserForm.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 function NewUserForm() {
   return(
   <Form>
-    <Form.Group controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
-      <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
-      </Form.Text>
+    <Form.Group controlId="formNewUserName">
+      <Form.Label>Nazwa</Form.Label>
+      <Form.Control type="text" placeholder="Podaj nazwę" />
     </Form.Group>
+    <Button variant="primary" type="submit">
+      Submit
+    </Button>
   </Form>
+  
   )
   }
 export default NewUserForm;
