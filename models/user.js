4e8b604bb39aa80a2b875b45ea03ce4b8de789b8
Wojciech Knott <wyoyo7@gmail.com>
2020-04-02 21:15:56 +0200

    Add user endpoints

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   }
 })
 
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+
 router.get('/', async (req,res) => {
 try {
-  const subscribers = await User.find()
+  const users = await User.find()
+  res.json(users)
 } catch (err) {
   res.status(500).json({message: err.message})
 } 
+})
+
+router.get('/:id', getUser, (req,res) => {
+  res.json(res.user)
 })
 
 router.post('/', async (req,res) => {
@@ -15,8 +21,41 @@ router.post('/', async (req,res) => {
     const newUser = await user.save()
     res.status(201).json(newUser)
   } catch (err) {
-    res.status(400).json({ message: err.message})
+    res.status(400).json({message: err.message})
   }
 })
 
+router.patch('/:id', getUser, async (req,res) => {
+  if (req.body.name != null) {
+    res.user.name = req.body.name
+  }
+  try {
+    const updatedUser = await res.user.save()
+    res.json(updatedUser)
+  } catch (err) {
+    res.status(400).json({message: err.message})
+  }
+})
+router.delete('/:id', getUser, async (req,res) => {
+  try {
+    await res.user.remove()
+    res.json({message: 'Deleted user'})
+  } catch (err) {
+    res.json(500).json({message: err.message})
+  }
+})
+
+async function getUser(req, res, next) {
+  let user
+  try {
+    user = await User.findById(req.params.id)
+    if (user == null) {
+       return res.status(404).json('Cannot find user')
+    }
+  } catch (err) {
+    return res.status(500).json({message: err.message})
+  }
+  res.user = user
+  next()
+}
 module.exports = router
