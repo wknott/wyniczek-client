68bee9da1ed68656a3d12c7081f54bf0b903d80d
Wojciech Knott <wyoyo7@gmail.com>
2020-04-22 22:21:40 +0200

    Heroku test 1

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ dotenv.config()
 dotenv.config()
 app.use(express.json())
 app.use(cors())
-
 app.use(express.static(path.join(__dirname, 'client/build')))
 const usersRouter = require('./routes/users')
 const gamesRouter = require('./routes/games')
@@ -19,7 +18,6 @@ app.use('/api/games', gamesRouter)
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'))
 })
-
 mongoose.connect(process.env.URL, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
 const db = mongoose.connection
 db.on('error', error => console.error(error))
