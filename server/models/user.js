8ee9a38cf86621353c79a15d2b43c6b62d75872c
Wojciech Knott <wyoyo7@gmail.com>
2020-05-02 17:46:44 +0200

    Add server folder

---
+++ server/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  hash: { 
+    type: String,
+    required: true
+  },
+})
+
+userSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+      delete ret._id;
+      delete ret.hash;
+  }
+});
+
+module.exports = mongoose.model('User', userSchema)
---
+++ server/routes/games.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const router = express.Router()
+const Game = require('../models/game')
+
+router.get('/', async (req,res) => {
+  try {
+    const games = await Game.find()
+    res.json(games)
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
+})
+
+router.get('/:id', getGame, (req,res) => {
+  res.json(res.game)
+})
+
+router.post('/', async (req,res) => {
+  const game = new Game({
+    name: req.body.name,
+    minPlayers: req.body.minPlayers,
+    maxPlayers: req.body.maxPlayers,
+    pointFields: req.body.pointFields
+  })
+  try {
+    const newGame = await game.save()
+    res.status(201).json(newGame)
+  } catch (err) {
+    res.status(400).json({message:err.message})
+  }
+})
+
+router.patch('/:id', getGame, async (req,res) => {
+  if (req.body.name != null) {
+    res.user.name = req.body.name
+  }
+  if (req.body.minPlayers != null) {
+    res.user.minPlayers = req.body.minPlayers
+  }
+  if (req.body.maxPlayers != null) {
+    res.user.maxPlayers = req.body.maxPlayers
+  }
+  if (req.body.pointFields != null) {
+    res.user.pointFields = req.body.pointFields
+  }
+  try {
+    const updatedGame = await res.game.save()
+    res.json(updatedGame)
+  } catch (err) {
+    res.status(400).json({message: err.message})
+  }
+})
+
+router.delete('/:id', getGame, async (req,res) => {
+  try {
+    await res.game.remove()
+    res.json({message: 'Deleted game'})
+  } catch (err) {
+    res.json(500).json({message: err.message})
+  }
+})
+
+async function getGame(req,res,next) {
+  let game 
+  try {
+    game = await Game.findById(req.params.id)
+    if (game == null) {
+      return res.status(404).json('Cannot find game')
+    }
+  } catch (err) {
+    return res.status(500).json({message: err.message})
+  }
+  res.game = game
+  next()
+}
+module.exports = router
