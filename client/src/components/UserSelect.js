cc2d6bfb6001d3e76e9caf371177c91e2f899668
Wojciech Knott <wyoyo7@gmail.com>
2020-05-10 18:21:17 +0200

    Modify onChangeScore function in UserSelect

--- client/src/components/UserSelect.js
+++ client/src/components/UserSelect.js
@@ -2,15 +2,16 @@ import React from 'react'
 import Form from 'react-bootstrap/Form'
 
 function UserSelect(props){
-  const {users,score,scores,setScores} = props
+  const {index, users,score,scores,setScores} = props
   function onChangeScore(e){
-    const newScore = {user:e.target.value, points:score.points}
+    const selectedUser = users.find(user => user.name === e.target.value)
+    const newScore = {user:selectedUser.id, points:score.points}
     const newScores = scores.map(s => (s === score)? newScore : s)
     setScores(newScores)
   }
   return(
     <Form.Control style={{minWidth:'80px'}} value={score.user === null? '': score.user._id} onChange={e => onChangeScore(e)} as="select">
-        <option value=''></option>
+        <option value=''>{index+1}. Gracz</option>
         {users.map((user,index) => (<option key={index} value={user._id}>{user.name}</option>))}
     </Form.Control>
   )
