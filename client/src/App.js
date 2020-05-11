91f60bfcfe4b7ed84e54a4c0d2f929e8ac7463b5
hobosoccer <22930956+hobosoccer@users.noreply.github.com>
2020-05-11 20:08:05 +0200

    Initial authentication state in App.

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,32 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from "react"
 import { AppContext } from "./libs/contextLib";
 import { BrowserRouter as Router, Route} from "react-router-dom"
-import "bootstrap/dist/css/bootstrap.min.css"
 import Wyniczek from "./components/Wyniczek"
 import Navigation from "./components/Navigation"
 import Users from "./components/Users"
 import Games from "./components/Games"
-import NewResultForm from './components/NewResultForm'
-import LoginForm from './components/LoginForm'
-import PrivateRoute from './components/PrivateRoute'
-import './App.css'
+import NewResultForm from "./components/NewResultForm"
+import LoginForm from "./components/LoginForm"
+import PrivateRoute from "./components/PrivateRoute"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+
+export const AuthContext = React.createContext();
+
 function App() {
   const [isAuthenticated, userHasAuthenticated] = useState(false);
   
+  useEffect(() => {    
+    const isUserInLocalStorage = localStorage.getItem("user") !== null
+    userHasAuthenticated(isUserInLocalStorage)
+  }, []);
+
   function handleLogout() {
     userHasAuthenticated(false);
     localStorage.removeItem("user")
   }
+
   return (
     <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
       <div className="container">
