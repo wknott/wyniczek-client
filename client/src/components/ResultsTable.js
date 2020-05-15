c639da01d1d44b7942f1eaf417a018c996c889d4
Wojciech Knott <wyoyo7@gmail.com>
2020-05-15 10:41:39 +0200

    Add first player columt to ResultsTable

--- client/src/components/ResultsTable.js
+++ client/src/components/ResultsTable.js
@@ -19,12 +19,23 @@ function ResultsTable(){
       return err
     }
   }
+  // function calculateWinner(results){
+  //   const sumOfPoints = results.map(result => 
+  //     Math.max(...(result.scores.map(score => 
+  //       Object.values(score.points).reduce((x, y) => x + y, 0))
+  //     )))
+  //   const winners = results.map(result =>
+  //     result.scores.find((score,index) => 
+  //       Object.values(score.points).reduce((x, y) => x + y, 0) === sumOfPoints[index]))
+  //   console.log(sumOfPoints[0])
+  // }
   async function loadResults(){
     try {
       const res = await fetch('/api/results', {
         headers: authHeader()
       })
       const results = await res.json()
+      //calculateWinner(results)
       setResults(results)
     } catch (err) {
       return err
@@ -43,6 +54,7 @@ function ResultsTable(){
           <tr>
             <td>#</td>
             <td>Gra</td>
+            <td>Pierwszy gracz</td>
             <td>Data</td>
           </tr>
         </thead>
@@ -52,6 +64,7 @@ function ResultsTable(){
               <tr key={index}>
                 <td>{index + 1}</td>
                 <td>{result.game.name}</td>
+                <td>{result.scores.find((score,index) => index === 0).user.name}</td>
                 <td>{formatDateStringShort(result.date)}</td>
                 <td>
                   <Button size="sm" disabled variant="danger" onClick={() => console.log(results)}>X</Button>
