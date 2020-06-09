56445cb81ded26a13fb15c22f80d3ae624fb1e39
Wojciech Knott <wyoyo7@gmail.com>
2020-06-09 23:56:59 +0200

    Add GameSelect to Stats

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -1,9 +1,36 @@
 import React, { useState, useEffect } from "react";
-
+import GameSelect from "./GameSelect";
+import { authHeader } from "../helpers/auth-header";
 export default function Stats() {
+  const [selectedGame, setSelectedGame] = useState();
+  const [games, setGames] = useState([]);
+  async function loadGames() {
+    try {
+      const res = await fetch("/api/games", {
+        headers: authHeader(),
+      });
+      const games = await res.json();
+      setGames(games);
+    } catch (err) {
+      return err;
+    }
+  }
+  function selectGame(e) {
+    const newSelectedGame = games.find((game) => game._id === e.target.value);
+    setSelectedGame(newSelectedGame);
+  }
+  useEffect(() => {
+    loadGames();
+  }, []);
   return (
     <div>
       <h3>Statystyki</h3>
+      <GameSelect
+        selectedGame={selectedGame}
+        selectGame={selectGame}
+        games={games}
+        firstOption={"Wybierz grę"}
+      />
     </div>
   );
 }
