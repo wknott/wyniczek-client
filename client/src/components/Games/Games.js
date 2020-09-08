393e0a3c8db7f58fd39881dd047a451fcf9ecd78
Wojciech Knott <wojciechknott@gmail.com>
2020-09-08 17:21:55 +0200

    removed newGameForm from games component

--- client/src/components/Games/Games.js
+++ client/src/components/Games/Games.js
@@ -1,40 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import GamesTable from "./GamesTable";
 import GameLastResultTable from "./GameLastResultTable";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
-import NewGameForm from "./NewGameForm";
 
-function Games() {
-  const [newGameTabIsActive, setNewGameTabIsActive] = useState(false);
-  const [isAuthenticated, userHasAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const isUserInLocalStorage = localStorage.getItem("user") !== null;
-    userHasAuthenticated(isUserInLocalStorage);
-  }, []);
-
-  function handleSelect(key) {
-    setNewGameTabIsActive(key === "newGame")
-  }
-
-  return (
-    <div>
-      <Tabs defaultActiveKey="lastResults" id="uncontrolled-tab-example" onSelect={(e) => handleSelect(e)}>
-        <Tab eventKey="gamesTable" title="Spis gier">
-          <h2 className="gamesHeader">Spis gier</h2>
-          <GamesTable />
-        </Tab>
-        <Tab eventKey="lastResults" title="Ostatnie wyniki">
-          <h2 className="gamesHeader">Liczba dni od ostatniego wyniku</h2>
-          <GameLastResultTable />
-        </Tab>
-        {isAuthenticated && <Tab eventKey="newGame" title="Nowa gra">
+const Games = () => (
+  <div>
+    <Tabs defaultActiveKey="lastResults" id="uncontrolled-tab-example">
+      <Tab eventKey="gamesTable" title="Spis gier">
+        <h2 className="gamesHeader">Spis gier</h2>
+        <GamesTable />
+      </Tab>
+      <Tab eventKey="lastResults" title="Ostatnie wyniki">
+        <h2 className="gamesHeader">Liczba dni od ostatniego wyniku</h2>
+        <GameLastResultTable />
+      </Tab>
+      {/* {isAuthenticated && <Tab eventKey="newGame" title="Nowa gra">
           <h2 className="gamesHeader">Nowa gra</h2>
           <NewGameForm isActive={newGameTabIsActive} />
-        </Tab>}
-      </Tabs>
-    </div>
-  );
-}
+        </Tab>} */}
+    </Tabs>
+  </div>
+);
+
 export default Games;
