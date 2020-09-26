9543d5a3987a8f0573a583c4d6b3ec3830b74e8c
Wojciech Knott <wojciechknott@gmail.com>
2020-09-26 22:41:49 +0200

    reversed dbUrl order

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.get('*', function (req, res) {
     }
   })
 })
-const dbUrl = "mongodb://localhost:27017/wyniczek" || process.env.MONGO_URL;
+const dbUrl = process.env.MONGO_URL || "mongodb://localhost:27017/wyniczek";
 mongoose.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
