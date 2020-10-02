f81571f55b6c3fca6e3e8bc8cd11e9edac2d165e
Wojciech Knott <wojciechknott@gmail.com>
2020-10-02 20:55:21 +0200

    installed react-loader, added Input to form

--- client/src/features/results/ResultsPage/ResultsTableSettingsForm/index.js
+++ client/src/features/results/ResultsPage/ResultsTableSettingsForm/index.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import ReactLoading from "react-loading";
 import Select from "../../../../common/Select";
+import Input from "../../../../common/Input";
 import { fetchGames, selectGames, selectLoading } from "../../../games/gamesSlice";
+import { theme } from "../../../../theme";
+import { compareObjects } from "../../../../logic/utilities";
 
 const ResultsTableSettingsForm = () => {
   const [selectedGame, setSelectedGame] = useState();
+  const [numberOfResults, setNumberOfResults] = useState(15);
   const games = useSelector(selectGames);
   const loading = useSelector(selectLoading);
+  const sortedGames = [...games].sort(compareObjects("name"));
 
   const dispatch = useDispatch();
 
@@ -19,19 +25,29 @@ const ResultsTableSettingsForm = () => {
   };
 
   return (
-    <form>
-      <label>Wybierz grę</label>
-      {
-        loading ?
-          "ładowanie" :
-          <Select
-            value={selectedGame}
-            onChange={onChange}
-            options={games}
-            firstOption={"Wszystkie gry"}
+    loading ?
+      <ReactLoading color={theme.colors.violet} /> :
+      <form>
+        <p>
+          <label>
+            Gra:
+            <Select
+              value={selectedGame}
+              onChange={onChange}
+              options={sortedGames}
+              firstOption={"Wszystkie gry"}
+            />
+          </label>
+        </p>
+        <label>
+          Liczba wyników:
+          <Input
+            type="number"
+            value={numberOfResults}
+            onChange={({ target }) => setNumberOfResults(target.value)}
           />
-      }
-    </form>
+        </label>
+      </form>
   );
 };
 
