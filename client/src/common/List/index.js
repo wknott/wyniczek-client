e6d9f031a0407e0a4c46d73412e870492fa46b19
Wojciech Knott <wojciechknott@gmail.com>
2020-10-29 11:57:25 +0100

    moved List styles to common components

---
+++ client/src/common/List/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.ul`
+  list-style-type: none;
+  padding: 10px 0px;
+`;
+
+export const ListItem = styled.li`
+  padding: 10px 0px;
+  cursor: pointer;
+`;
