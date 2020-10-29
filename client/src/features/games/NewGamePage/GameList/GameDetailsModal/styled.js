0f400fad830c07353400ee142f3ddc659732d274
Wojciech Knott <wojciechknott@gmail.com>
2020-10-29 13:00:29 +0100

    added styles to ButtonContainer

--- client/src/features/games/NewGamePage/GameList/GameDetailsModal/styled.js
+++ client/src/features/games/NewGamePage/GameList/GameDetailsModal/styled.js
@@ -4,3 +4,10 @@ export const GameImage = styled.img`
   width: 100%;
   margin-bottom: 10px;
 `;
+
+export const ButtonsContainer = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  margin-bottom: 10px;
+  grid-template-columns: 1fr 1fr;
+`;
