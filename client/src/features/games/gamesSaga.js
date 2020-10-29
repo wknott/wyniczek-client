edc3192905832525f9dbcead2d83937e1322edf2
Wojciech Knott <wojciechknott@gmail.com>
2020-10-29 11:59:45 +0100

    added new actions and state to gameSlice, added fetchGamesFromQuery handler to gameSaga

--- client/src/features/games/gamesSaga.js
+++ client/src/features/games/gamesSaga.js
@@ -1,6 +1,14 @@
-import { takeLatest, call, put } from "redux-saga/effects";
-import { getGames } from "../../proxy/api";
-import { fetchGames, fetchGamesError, fetchGamesSuccess } from "./gamesSlice";
+import { takeLatest, call, put, debounce } from "redux-saga/effects";
+import { getGameDetails, getGames, getGamesFromQuery } from "../../proxy/api";
+import {
+  fetchGames,
+  fetchGamesByQuery,
+  fetchError,
+  fetchGamesByQuerySuccess,
+  fetchGamesSuccess,
+  fetchGameDetails,
+  fetchGameDetailsSuccess
+} from "./gamesSlice";
 
 function* fetchGamesHandler() {
   try {
@@ -8,10 +16,32 @@ function* fetchGamesHandler() {
     yield put(fetchGamesSuccess(games));
   } catch (error) {
     yield call(alert, "Nie udało się wczytać gier, spróbuj odświeżyć stronę.");
-    yield put(fetchGamesError());
+    yield put(fetchError());
+  }
+}
+
+function* fetchGameDetailsHandler({ payload: gameId }) {
+  try {
+    const gameDetails = yield call(getGameDetails, gameId);
+    yield put(fetchGameDetailsSuccess(gameDetails));
+  } catch (error) {
+    yield call(alert, "Nie udało się wczytać gier, spróbuj odświeżyć stronę.");
+    yield put(fetchError());
+  }
+}
+
+function* fetchGamesByQueryHandler({ payload: query }) {
+  try {
+    const games = yield call(getGamesFromQuery, query);
+    yield put(fetchGamesByQuerySuccess(games));
+  } catch (error) {
+    yield call(alert, "Nie udało się wczytać gier, spróbuj odświeżyć stronę.");
+    yield put(fetchError());
   }
 }
 
 export function* watchFetchGames() {
   yield takeLatest(fetchGames.type, fetchGamesHandler);
+  yield takeLatest(fetchGameDetails.type, fetchGameDetailsHandler);
+  yield debounce(500, fetchGamesByQuery.type, fetchGamesByQueryHandler);
 };
--- client/src/features/games/gamesSlice.js
+++ client/src/features/games/gamesSlice.js
@@ -5,6 +5,9 @@ const gamesSlice = createSlice({
   initialState: {
     games: [],
     loading: false,
+    query: "",
+    foundGames: [],
+    gameDetails: null,
   },
   reducers: {
     fetchGames: state => {
@@ -14,16 +17,46 @@ const gamesSlice = createSlice({
       state.games = games;
       state.loading = false;
     },
-    fetchGamesError: state => {
+    fetchGameDetails: state => {
+      state.loading = true;
+    },
+    fetchGameDetailsSuccess: (state, { payload: gameDetails }) => {
+      state.gameDetails = gameDetails;
       state.loading = false;
     },
+    setQuery: (state, { payload: query }) => {
+      state.query = query;
+    },
+    fetchGamesByQuery: state => {
+      state.loading = true;
+    },
+    fetchGamesByQuerySuccess: (state, { payload: foundGames }) => {
+      state.foundGames = foundGames;
+      state.loading = false;
+    },
+    fetchError: state => {
+      state.loading = false;
+    },
+
   },
 });
 
-export const { fetchGames, fetchGamesError, fetchGamesSuccess } = gamesSlice.actions;
+export const {
+  fetchGames,
+  fetchError,
+  fetchGamesSuccess,
+  fetchGamesByQuery,
+  fetchGamesByQuerySuccess,
+  setQuery,
+  fetchGameDetails,
+  fetchGameDetailsSuccess,
+} = gamesSlice.actions;
 
 export const selectGamesState = state => state.games;
 export const selectGames = state => selectGamesState(state).games;
 export const selectLoading = state => selectGamesState(state).loading;
+export const selectQuery = state => selectGamesState(state).query;
+export const selectFoundGames = state => selectGamesState(state).foundGames;
+export const selectGameDetails = state => selectGamesState(state).gameDetails;
 
 export default gamesSlice.reducer;
