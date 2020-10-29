e98c0fb078823d8db51b648825d13d572a6dcaf4
Wojciech Knott <wojciechknott@gmail.com>
2020-10-29 13:00:16 +0100

    added new game form to modal

--- client/src/features/games/NewGamePage/GameList/GameDetailsModal/index.js
+++ client/src/features/games/NewGamePage/GameList/GameDetailsModal/index.js
@@ -1,15 +1,62 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import { useDispatch, useSelector } from "react-redux";
 import ReactLoading from "react-loading";
 import { fetchGameDetails, selectGameDetails, selectLoading } from "../../../gamesSlice";
 import { theme } from "../../../../../theme";
-import { GameImage } from "./styled";
+import { GameImage, ButtonsContainer } from "./styled";
+import Button from "../../../../../common/Button";
+import Label from "../../../../../common/Label";
+import Input from "../../../../../common/Input";
+import { StyledSelect } from "../../../../../common/Select/styled";
+import { nanoid } from "@reduxjs/toolkit";
+import { authHeader } from "../../../../../helpers/auth-header";
 
 function GameDetailsModal({ show, handleClose, gameId }) {
   const gameDetails = useSelector(selectGameDetails);
   const loading = useSelector(selectLoading);
   const dispatch = useDispatch();
+  const [pointFields, setPointFields] = useState([]);
+  const [selectedName, setSelectedName] = useState("");
+
+  const addField = () => {
+    setPointFields([...pointFields, ""]);
+  }
+
+  const deleteField = () => {
+    setPointFields(
+      pointFields.filter((item, index) => index !== pointFields.length - 1)
+    );
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    const name = selectedName;
+    const minPlayers = gameDetails.minPlayers;
+    const maxPlayers = gameDetails.maxPlayers;
+
+    const newGame = { name, minPlayers, maxPlayers, pointFields };
+    const authToken = authHeader()["Authorization"];
+
+    try {
+      const res = await fetch("/api/games", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: authToken,
+        },
+        body: JSON.stringify(newGame),
+      });
+      const data = await res.json();
+      setSelectedName("");
+      setPointFields([]);
+      handleClose();
+      return data;
+    } catch (err) {
+      return err;
+    }
+  }
 
   useEffect(() => {
     if (gameId) {
@@ -40,10 +87,50 @@ function GameDetailsModal({ show, handleClose, gameId }) {
                 {" "}{gameDetails.name[0]}
               </a>
             </p>
+            <ButtonsContainer>
+              <Button onClick={addField}>
+                Dodaj kategorię
+                </Button>
+              <Button onClick={deleteField}>
+                Usuń kategorię
+                </Button>
+            </ButtonsContainer>
+            <form onSubmit={onSubmit}>
+              {pointFields.map((field, key) => (
+                <Label labelText={`${key + 1}. kategoria`}>
+                  <Input
+                    key={key}
+                    type="text"
+                    value={field}
+                    onChange={(e) =>
+                      setPointFields(
+                        pointFields.map((name, index) =>
+                          index === key ? e.target.value : name
+                        )
+                      )
+                    }
+                  />
+                </Label>
+              ))}
+              <Label labelText="Nazwa gry">
+                <StyledSelect
+                  value={selectedName}
+                  onChange={({ target }) => setSelectedName(target.value)}
+                >
+                  <option>Wybierz nazwę gry</option>
+                  {gameDetails.name.map(name =>
+                    <option key={nanoid()} value={name}>
+                      {name}
+                    </option>)}
+                </StyledSelect>
+              </Label>
+              <Button type="submit">Dodaj do listy gier</Button>
+            </form>
+
           </>
         }
       </Modal.Body>
-    </Modal>
+    </Modal >
   )
 };
 export default GameDetailsModal;
