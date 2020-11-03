a9a1b6e339390ecdbeed8611651b59388872aba6
Wojciech Knott <wojciechknott@gmail.com>
2020-11-03 16:29:38 +0100

    mofied game model

--- server/models/game.js
+++ server/models/game.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose')
 
 const gameSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true
-  },
+  name: { type: String, required: true, unique: true },
   minPlayers: { type: Number, required: true, min: 1, max: 99 },
-  maxPlayers: { type: Number, required: true, min: 1, max: 99},
-  pointFields: { type: [{ type: String }], required: true }
+  maxPlayers: { type: Number, required: true, min: 1, max: 99 },
+  pointFields: { type: [{ type: String }], required: true },
+  bggId: { type: Number },
+  imgUrl: { type: String },
+  thumbnailUrl: { type: String },
 })
 
 module.exports = mongoose.model('Game', gameSchema)
