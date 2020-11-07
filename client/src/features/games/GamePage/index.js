da9007981d359cf4cd4ffdee24383724d5f5e8b8
Wojciech Knott <wojciechknott@gmail.com>
2020-11-07 14:56:36 +0100

    created game page component

---
+++ client/src/features/games/GamePage/index.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ReactLoading from "react-loading";
+import LinkToWebsite from "../../../common/LinkToWebsite";
+import Section from "../../../common/Section";
+import { selectAuth } from "../../../authSlice";
+import { useParams } from "react-router-dom";
+import { fetchGames, getGameById, selectGames } from "../gamesSlice";
+import { theme } from "../../../theme";
+import meeple from "../../../images/meeple.svg";
+import { Image, DefaultImage, DefaultImageContainer } from "./styled";
+
+const GamePage = () => {
+  const { isAuthenticated } = useSelector(selectAuth);
+  const { id } = useParams();
+  const games = useSelector(selectGames);
+  const dispatch = useDispatch();
+  const game = useSelector(state => getGameById(state, id));
+  console.log(games);
+  useEffect(() => {
+    if (games.length < 1) {
+      dispatch(fetchGames())
+    }
+  }, [dispatch, games]);
+  return (
+    game ?
+      <Section>
+        {
+          game.imgUrl ?
+            <Image src={game.imgUrl} alt="game" /> :
+            <DefaultImageContainer>
+              <DefaultImage src={meeple} alt="meeple" />
+            </DefaultImageContainer>
+        }
+        <h1>{game.name}</h1>
+        <p>Link do opisu gry w serwisie BGG:
+        {game.bggId ? <LinkToWebsite
+            target="_blank"
+            rel="noopener noreferrer"
+            href={`https://boardgamegeek.com/boardgame/${game.bggId}`}
+          >
+            game.name
+          </LinkToWebsite> : "brak linku"}
+        </p>
+      </Section> :
+      <ReactLoading color={theme.colors.violet} />
+  )
+}
+
+export default GamePage;
---
+++ client/src/features/games/GamePage/styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import Section from "../../../common/Section";
+
+export const Image = styled.img`
+  width: 200px;
+  border-radius: 5px;
+  object-fit: contain;
+`;
+
+export const DefaultImage = styled.img`
+  width: 100px;
+`;
+
+export const DefaultImageContainer = styled.div`
+  width: 200px;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.darkGrey};
+  border-radius: 5px;
+  object-fit: contain;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const StyledSection = styled(Section)`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-gap: 12px;
+`;
