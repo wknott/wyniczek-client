b3a46a69ccace54ea1a83632ed599b71f5a0fdb5
Wojciech Knott <wojciechknott@gmail.com>
2020-11-09 22:25:32 +0100

    removed section, added header component

--- client/src/features/results/ResultsPage/index.js
+++ client/src/features/results/ResultsPage/index.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import Section from "../../../common/Section";
 import ResultsTableSettingsForm from "./ResultsTableSettingsForm";
 import ResultsTable from "./ResultsTable"
 import { useSelector } from "react-redux";
 import { selectLoading } from "../../games/gamesSlice";
+import Header from "../../../common/Header";
 const ResultsPage = () => {
   const [selectedGame, setSelectedGame] = useState();
   const [numberOfResults, setNumberOfResults] = useState(15);
@@ -11,20 +11,16 @@ const ResultsPage = () => {
 
   return (
     <>
-      <Section sectionHeader="Historia wyników">
-        <p>
-          W poniższym formularzu wybierz grę, której wyniki chcesz
-          zobaczyć, a następnie podaj liczbę wyników, która ma zostać wyświetlona w tabeli.
-          Jeśli chcesz zobaczyć szczegóły danego wyniku, kliknij w odpowiedni wiersz.
-        </p>
-        <ResultsTableSettingsForm
-          selectedGame={selectedGame}
-          setSelectedGame={setSelectedGame}
-          numberOfResults={numberOfResults}
-          setNumberOfResults={setNumberOfResults}
-        />
-        {loading || <ResultsTable numberOfResults={numberOfResults} selectedGame={selectedGame} />}
-      </Section>
+      <Header>
+        Historia wyników
+      </Header>
+      <ResultsTableSettingsForm
+        selectedGame={selectedGame}
+        setSelectedGame={setSelectedGame}
+        numberOfResults={numberOfResults}
+        setNumberOfResults={setNumberOfResults}
+      />
+      {loading || <ResultsTable numberOfResults={numberOfResults} selectedGame={selectedGame} />}
     </>
   )
 };
