e9375b06cf9069f4a292accc195c4ad749e06a3a
Wojciech Knott <wojciechknott@gmail.com>
2020-11-09 20:09:03 +0100

    added Tile component with common styles

---
+++ client/src/common/Tile/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.div`
+  box-shadow: 0px 4px 12px ${({ theme }) => theme.colors.windsor};
+  border: 1px solid ${({ theme }) => theme.colors.alto};
+  border-radius: 6px;
+  padding: 24px;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-gap: 24px;
+`;
