428f186b2ad07cc062ba8921df4909094d5379b7
Wojciech Knott <wojciechknott@gmail.com>
2020-11-09 20:08:48 +0100

    added styles to List component

--- client/src/common/List/index.js
+++ client/src/common/List/index.js
@@ -2,10 +2,17 @@ import styled from "styled-components";
 
 export default styled.ul`
   list-style-type: none;
-  padding: 10px 0px;
+  padding: 10px 20px;
 `;
 
 export const ListItem = styled.li`
   padding: 10px 0px;
+  color: ${({ theme }) => theme.colors.black};
+  text-decoration: none;
+  display: block;
   cursor: pointer;
+
+  &:hover{
+    color: ${({ theme }) => theme.colors.black};
+  }
 `;
