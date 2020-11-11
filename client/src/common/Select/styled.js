1b9e1a57118a42e90c86890fb5aad3e35b0afecc
Wojciech Knott <wojciechknott@gmail.com>
2020-11-11 17:50:11 +0100

    removed max width from select

--- client/src/common/Select/styled.js
+++ client/src/common/Select/styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const StyledSelect = styled.select`
   width: 100%;
-  max-width: 400px;
   padding: 10px;
   border: 1px solid ${({ theme }) => theme.colors.windsor};
   border-radius: 5px;
