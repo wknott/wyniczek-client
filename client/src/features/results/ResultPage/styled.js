d5b580905d187ef14b9e366b02fbef6bc8363999
Wojciech Knott <wojciechknott@gmail.com>
2020-11-13 17:40:53 +0100

    added winner and first player to result page

--- client/src/features/results/ResultPage/styled.js
+++ client/src/features/results/ResultPage/styled.js
@@ -18,7 +18,8 @@ export const Info = styled.div`
   flex-direction: column;
 
   @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-    flex-direction: row;
+    display: grid;
+    grid-template-columns: auto 1fr;
   }
 `;
 
@@ -26,14 +27,58 @@ export const Image = styled.img`
   width: 300px;
 
   @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-    max-width: 64px;
+    max-width: 100px;
+    margin-right: 8px;
   }
 `;
 
 export const GameName = styled(Link)`
+  text-align: center;
+  font-size: 24px;
+  padding: 8px;
   text-decoration: none;
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.windsor};
+  border-radius: 5px;
+  overflow-wrap: anyware;
+  max-width: 300px;
+
+  &:hover{
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.white};
+    filter: brightness(110%);
+  }
+
+  @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 16px;
+    padding: 4px;
+    max-width: 100px;
+  }
 `;
 
 export const Date = styled.p`
   margin: 0;
+  color: ${({ theme }) => theme.colors.windsor};
+`;
+
+export const Winner = styled.p`
+  font-size: 24px;
+  margin: 0;
+  @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 16px;
+  }
+`;
+
+export const FirstPlayer = styled.p`
+margin: 0;
+`;
+
+export const Icon = styled.img`
+  height: 24px;
+  margin-right: 8px;
+`;
+
+export const Game = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
 `;
