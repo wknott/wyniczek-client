c95c987d67bed766b6d6d7117a58c17cbd74c1cd
Wojciech Knott <wojciechknott@gmail.com>
2020-11-16 14:28:07 +0100

    added red color

--- client/src/theme.js
+++ client/src/theme.js
@@ -4,6 +4,7 @@ export const theme = {
     gallery: "#EEEEEE",
     alto: "#DDDDDD",
     silver: "#CCCCCC",
+    red: "#E60010",
     amber: "#FFBF00",
     windsor: "#5F0A87",
     pink: "#FFC7F4",
