7f9990d1d2943ec9dced008880772f669f2add8c
Wojciech Knott <wojciechknott@gmail.com>
2020-11-22 23:40:50 +0100

    added styles to a link

--- client/src/common/LinkToWebsite/index.js
+++ client/src/common/LinkToWebsite/index.js
@@ -2,4 +2,13 @@ import styled from "styled-components";
 
 export default styled.a`
   display: inline;
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.windsor};
+  padding: 4px;
+  border-radius: 2px;
+  transition: 0.3s;
+  
+  &:hover {
+    filter: brightness(120%);
+  }
 `;
