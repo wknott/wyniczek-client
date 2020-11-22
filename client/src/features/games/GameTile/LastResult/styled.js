c696d102c4bfdb1dcd5ec808796d61cc9adf72e2
Wojciech Knott <wojciechknott@gmail.com>
2020-11-22 13:37:43 +0100

    moved to games feature folder

--- client/src/features/games/GameTile/LastResult/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledParagraph = styled.p`
-  font-size: 14px;
-  background-color: ${({ theme, color }) => theme.colors[color]};
-  border-radius: 5px;
-  padding: 5px;
-  margin: 0;
-`; 
