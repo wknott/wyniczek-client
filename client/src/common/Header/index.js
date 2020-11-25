0cefca35c4c4d17c8192b1c86b69714122c065f7
Wojciech Knott <wojciechknott@gmail.com>
2020-11-25 10:22:46 +0100

    added mobile styles to Header

--- client/src/common/Header/index.js
+++ client/src/common/Header/index.js
@@ -2,5 +2,10 @@ import styled from "styled-components";
 
 export default styled.h1`
   font-size: 32px;
-  margin-top: 24px;
+  margin: 24px 0px 14px;
+
+  @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px){
+    font-size: 24px;
+    margin: 12px 0px 2px;
+  }
 `;
