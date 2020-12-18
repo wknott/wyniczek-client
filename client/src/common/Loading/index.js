f2811476e79aca6e3068d3dbe4120f9e0727c61b
Wojciech Knott <wojciechknott@gmail.com>
2020-12-18 23:10:37 +0100

    added loading component

---
+++ client/src/common/Loading/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { theme } from "../../theme";
+import ReactLoading from "react-loading";
+import { Container, Message } from "./styled";
+
+const Loading = ({ message, size }) => (
+  <Container>
+    {message && <Message>{message}</Message>}
+    <div>
+      <ReactLoading
+        type="spinningBubbles"
+        color={theme.colors.windsor}
+        height={`${size || 128}px`}
+        width={`${size || 128}px`}
+      />
+    </div>
+  </Container>
+);
+
+export default Loading;
---
+++ client/src/common/Loading/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Message = styled.p`
+  font-size: 20px;
+  line-height: 140%;
+  letter-spacing: 0.05em;
+  margin: 0 0 48px;
+`;
