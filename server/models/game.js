41a718c2e64ef84611a070c4ff71e5f18a14283f
Wojciech Knott <wojciechknott@gmail.com>
2021-01-14 20:26:34 +0100

    removed maximum in game number of players schema

--- server/models/game.js
+++ server/models/game.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const gameSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  minPlayers: { type: Number, required: true, min: 1, max: 99 },
-  maxPlayers: { type: Number, required: true, min: 1, max: 99 },
+  minPlayers: { type: Number, required: true, min: 1 },
+  maxPlayers: { type: Number, required: true, min: 1 },
   pointFields: { type: [{ type: String }], required: true },
   bggId: { type: Number },
   imgUrl: { type: String },
