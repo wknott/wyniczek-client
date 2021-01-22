eb83d4ee2ab87e33bcdc4ee61ceb66caf91b466c
Wojciech Knott <wojciechknott@gmail.com>
2021-01-22 22:06:06 +0100

    added intearction to input, submit button

--- client/src/common/Edit/index.js
+++ client/src/common/Edit/index.js
@@ -1,31 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyledInput, Button, Image, Container } from "./styled";
 import confirmImage from "./confirm.svg";
 import denyImage from "./deny.svg";
-import { selectResult } from "../../features/results/resultsSlice";
+import { changeResult, selectResult } from "../../features/results/resultsSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 const Edit = ({ setEditCell, score, indexk }) => {
+  const [newScore, setNewScore] = useState(score);
   const result = useSelector(selectResult);
   const dispatch = useDispatch();
-  const onChange = (event) => {
-    console.log(event.target.value);
-    const newResult = {
-      ...result, scores: result.scores.map(
-        oldScore => oldScore === score ?
-          {
-            ...oldScore, points: score.points.map(
-              (point, index) => index === indexk ? !!event.target.value : point)
-          } : oldScore
-      )
-    };
-    console.log(newResult);
 
+  const onChange = (event) => {
+    const editedScore = {
+      ...newScore, points: newScore.points.map(
+        (point, index) => index === indexk ? +event.target.value : point)
+    };
+    setNewScore(editedScore);
   }
+
+  const onSubmit = () => {
+    console.log(newScore)
+    const newResult = {
+      ...result,
+      scores: result.scores.map(score => score._id === newScore._id ? newScore : score),
+    };
+    dispatch(changeResult(newResult));
+    setEditCell(null)
+  }
+
   return (
     <Container>
-      <StyledInput type="number" onChange={(event) => onChange(event)} value={score.points[indexk] || "0"} />
-      <Button><Image src={confirmImage} /></Button>
+      <StyledInput type="number" onChange={(event) => onChange(event)} value={newScore.points[indexk] || "0"} />
+      <Button onClick={onSubmit}><Image src={confirmImage} /></Button>
       <Button onClick={() => setEditCell(null)}><Image src={denyImage} /></Button>
     </Container>
   );
